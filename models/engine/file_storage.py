d142d32781b417a0fdd46317db66c626dd225275
yassine4real <yssnabbou2002@gmail.com>
2024-05-18 16:50:14 +0100

    fix pycodestyle errors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,11 +10,13 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
+
 class FileStorage:
     """Represent an abstracted storage engine.
+
     Attributes:
-    __file_path (str): The name of the file to save objects to.
-    __objects (dict): A dictionary of instantiated objects.
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
     """
     __file_path = "file.json"
     __objects = {}
@@ -22,20 +24,23 @@ class FileStorage:
     def all(self):
         """Return the dictionary __objects."""
         return FileStorage.__objects
+
     def new(self, obj):
         """Set in __objects the obj with key <obj class name>.id"""
         obj_clname = obj.__class__.__name__
         FileStorage.__objects["{}.{}".format(obj_clname, obj.id)] = obj
-        
+
     def save(self):
         """Serialize __objects to the JSON file __file_path"""
         odict = FileStorage.__objects
         objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
         with open(FileStorage.__file_path, 'w') as json_file:
-            json.dump(objdict , json_file)
+            json.dump(objdict, json_file)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects (if it exists)."""
+        """
+        Deserialize the JSON file __file_path to __objects (if it exists).
+        """
         try:
             with open(FileStorage.__file_path, 'r') as json_file:
                 objdict = json.load(json_file)
@@ -44,6 +49,4 @@ class FileStorage:
                     del obj["__class__"]
                     self.new(eval(clname)(**obj))
         except FileNotFoundError:
-            return 
-
-
+            return
